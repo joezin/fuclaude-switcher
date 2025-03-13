db7925e23aea9e204c5a52f2b05c010783a8db23
liujuntao <liujuntao@chinamobile.com>
2025-03-13 15:31:57 +0800

    feat: 修改sql

--- schema.sql
+++ schema.sql
@@ -6,10 +6,11 @@ CREATE TABLE accounts (
   id SERIAL PRIMARY KEY,
   user_id TEXT NOT NULL,
   name TEXT NOT NULL,
-  email TEXT NOT NULL UNIQUE,
+  email TEXT NOT NULL,
   session_key TEXT NOT NULL,
   prefix_url TEXT NOT NULL,
   is_active BOOLEAN NOT NULL DEFAULT TRUE,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE (user_id, name, email)
 ); 
